f075734844f4c65a592c05e018dfa784866b5b34
ewaudo <ewaudo@copiakenya.com>
2021-12-26 12:26:39 +0300

    rename module to

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "fran_salon",
+    'name': "Fran Beauty Parlour",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
