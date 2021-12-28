2a585b551e8af12504ad4c317e1cf9054d2a597a
ewaudo <ewaudo@copiakenya.com>
2021-12-28 12:54:56 +0300

    add checks for lower and upper

--- models/salon_management.py
+++ models/salon_management.py
@@ -83,3 +83,45 @@ class SalonService(models.Model):
     _inherit = 'salon.service'
 
     salon_service_category_id = fields.Many2one('salon.service.category', required=True)
+    price_lower = fields.Monetary(string="Price (Lower)")
+    price = fields.Monetary(string="Price (Upper)")
+
+    def _validate_price(self, lower, upper):
+        if not float(lower) <= float(upper):
+            raise ValidationError(
+                _(f"Service {self.name}: Lower Price {lower} can't be higher than {upper}"))
+
+    def write(self, vals):
+        name = vals.get('name', None)
+        lower_price = vals.get('price_lower', None)
+        upper_price = vals.get('price', None)
+        if lower_price and upper_price:
+            pass
+        elif lower_price:
+            upper_price = self.price
+        elif upper_price:
+            lower_price = self.price_lower
+
+        if upper_price or lower_price:
+            self._validate_price(lower_price, upper_price)
+
+        if name:
+            vals['name'] = name.upper()
+        write_res = super(SalonService, self).write(vals)
+        return write_res
+
+    @api.model
+    def create(self, vals):
+        name = vals.get('name', None)
+        salon_service_category_id = vals.get('salon_service_category_id', None)
+        if name and salon_service_category_id:
+            name = name.upper()
+            names = self.search(
+                [('name', '=', name), ('salon_service_category_id', '=', int(salon_service_category_id))])
+            if names:
+                salon_service_category = self.env['salon.service.category'].browse(int(salon_service_category_id))
+                raise ValidationError(
+                    _(f"Service ({name}) Already exists in Category {salon_service_category.name}"))
+            vals['name'] = name
+            res = super().create(vals)
+            return res
